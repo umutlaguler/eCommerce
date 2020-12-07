91d2433a2f84addd4e638eee019a71bfa54dcddb
perseusss <ed>
2020-12-07 15:00:39 +0300

    font family setted

--- src/components/pages/campaign.js
+++ src/components/pages/campaign.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity,Dimensions,Image,} from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { FlatList } from 'react-native-gesture-handler';
-// import GlobalFont from 'react-native-global-font';
+import GlobalFont from 'react-native-global-font';
 
 const data = [
-  { id: "1", title: "this is a campaign", backgroundColor: '#fefddb',uri:'https://envato-shoebox-0.imgix.net/0d80/552a-aceb-4181-bf08-49c70144d9e5/Goods-35.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=2f6fc6c7c3d02481ddd5a74a401fc4f7' },
-  { id: "2", title: "this is a campaign", backgroundColor: '#fbe8e7',uri:'https://envato-shoebox-0.imgix.net/8f42/9f4c-983e-4bf6-a731-22aaa077a810/IMG_7912.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=ee3910220eb35658fe4081fff890a79d' },
-  { id: "3", title: "this is a campaign", backgroundColor: '#f2e5fd',uri:'https://envato-shoebox-0.imgix.net/673e/e016-3db3-46fd-880e-c15f03db6666/smporidge3.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=b7faa4619db6f2eba586f9a7fb36c98c' },
-  { id: "4", title: "this is a campaign", backgroundColor: '#dff3fe',uri:'https://envato-shoebox-0.imgix.net/d7ee/ab79-15b1-4b4e-99ad-acbf947f3f0e/200102571A3173.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=90b6e1975a18ceee89f018fbc31c328d' },
-  { id: "5", title: "this is a campaign", backgroundColor: '#f2e5fd',uri:'https://envato-shoebox-0.imgix.net/3c7a/f0a0-ffd7-4060-9305-00d5cb2e6c85/44986.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=bd9eaecbaa83cecfbd3fef6ee50a6170' },
+  { id: "1", title: "temizlikte indirim", backgroundColor: '#fefddb',uri:'https://envato-shoebox-0.imgix.net/0d80/552a-aceb-4181-bf08-49c70144d9e5/Goods-35.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=2f6fc6c7c3d02481ddd5a74a401fc4f7' },
+  { id: "2", title: "Kıyafetlerde indirim", backgroundColor: '#fbe8e7',uri:'https://envato-shoebox-0.imgix.net/8f42/9f4c-983e-4bf6-a731-22aaa077a810/IMG_7912.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=ee3910220eb35658fe4081fff890a79d' },
+  { id: "3", title: "meyve ve sebzelerde indirim", backgroundColor: '#f2e5fd',uri:'https://envato-shoebox-0.imgix.net/673e/e016-3db3-46fd-880e-c15f03db6666/smporidge3.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=b7faa4619db6f2eba586f9a7fb36c98c' },
+  { id: "4", title: "Kozmetikte indirim", backgroundColor: '#dff3fe',uri:'https://envato-shoebox-0.imgix.net/d7ee/ab79-15b1-4b4e-99ad-acbf947f3f0e/200102571A3173.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=90b6e1975a18ceee89f018fbc31c328d' },
+  { id: "5", title: "Kitaplarda indirim", backgroundColor: '#f2e5fd',uri:'https://envato-shoebox-0.imgix.net/3c7a/f0a0-ffd7-4060-9305-00d5cb2e6c85/44986.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1600&s=bd9eaecbaa83cecfbd3fef6ee50a6170' },
 ]
 
 const PhoneWidth  = Dimensions.get("window").width;
@@ -22,10 +22,10 @@ export default class campaign extends Component {
     };
   }
 
-// componentDidMount() {
-//     let fontName = 'AbrilFatface-Regular'
-//     GlobalFont.applyGlobal(fontName)
-// }
+componentDidMount() {
+    let fontName = 'Segoe UI'
+    GlobalFont.applyGlobal(fontName)
+}
   campaignRenderItem=({item}) => {
     return(
     <View style = {styles.campaignBox}>
@@ -66,7 +66,10 @@ const styles = StyleSheet.create({
   campaignTxt:{
     position:'absolute',
     zIndex:22,
-    textAlign:'left'
+    textAlign:'left',
+    fontFamily:'Segoe UI',
+    fontSize:25,
+    color:'white'
   },
   body:{
     borderWidth:0,
--- src/components/pages/others.js
+++ src/components/pages/others.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { View, Text,StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { PhoneHeight, PhoneWidth } from '../config/env';
+import GlobalFont from 'react-native-global-font';
 
 export default class others extends Component {
   constructor(props) {
@@ -10,6 +11,10 @@ export default class others extends Component {
     this.state = {
     };
   }
+  componentDidMount() {
+    let fontName = 'Segoe UI'
+    GlobalFont.applyGlobal(fontName)
+}
 
   render() {
     return (
@@ -41,37 +46,36 @@ const styles = StyleSheet.create({
     marginTop:20
   },
   seperateBtn:{
-    alignItems:'center',
     borderWidth:1.5,
     borderColor:'#FE5F55',
     borderRadius:45,
     height:PhoneHeight*0.07,
     width:PhoneWidth*0.7,
-    marginVertical:8
+    marginVertical:8,
+    justifyContent:'center'
   },
   btnTxt:{
     fontSize:20,
     alignSelf:'center',
     color:'#FE5F55',
-    marginTop:13
   },
   logOutBtn:{
     marginTop:'20%'
   },
   seperateLogOut:{
     borderColor:'#8F8F8F',
-    alignItems:'center',
     borderRadius:45,
     borderWidth:1.5,
     height:PhoneHeight*0.07,
     width:PhoneWidth*0.5,
+    justifyContent:'center'
   },
   logOutbtnTxt:{
     color:'#8F8F8F',
     fontSize:20,
     textAlign:'center',
     alignSelf:'center',
-    marginTop:11
+
   }
 
 })
